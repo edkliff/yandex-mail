3932f1195ec2e17acfd28d0ecd74b8f1aab74343
edkliff <edkliff@gmail.com>
2018-02-01 12:49:02 +0300

    Create config.py
    Config template

---
+++ config.py
@@ -0,0 +1,8 @@
+DOMAIN_KEY = {'domain_name_inside1': ('domain_name_in_yandex', 'yandex_pdd_api_key'),
+              'domain_name_inside2': ('domain_name_in_yandex', 'yandex_pdd_api_key'),
+              ...
+              'domain_name_insideN': ('domain_name_in_yandex', 'yandex_pdd_api_key')}
+
+
+class Config(object):
+    SECRET_KEY = 'your-secret-key'
